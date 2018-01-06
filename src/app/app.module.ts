d4abdd76b2f23046aa0a6888f9aa3b0b8e68a001
Feng Lin <lin.feng55@hotmail.com>
2018-01-06 18:35:54 +0100

    parking service toegevoed met daarin interface parking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http/';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    HttpClientModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
--- src/app/parking.service.ts
+++ src/app/parking.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable()
 export class ParkingService {
-
-  constructor() { }
+  constructor( private _http:HttpClient) { }
 
 }
+
+export interface Parking{
+   
+       id: number,
+       objectid: number,
+       point_lat: string,
+       point_lng: string,
+       aantal_plaatsen: string,
+       kleur: string,
+       jrnr_gr_cbs: string,
+       adres: string,
+       start_date: string,
+       gisid: string 
+  
+
+  }
